298f5ae9881b25c2eacc95fbf2b4653150c27127
Misbah Ansori <misbah.ansori24@gmail.com>
2025-03-06 07:12:45 +0700

    Fix : sidebar

--- app/components/ui/sidebar/SidebarMenuSkeleton.vue
+++ app/components/ui/sidebar/SidebarMenuSkeleton.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils'
-import { Skeleton } from '@/components/ui/skeleton'
-import { computed, type HTMLAttributes } from 'vue'
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
+import { computed, type HTMLAttributes } from "vue";
 
 const props = defineProps<{
-  showIcon?: boolean
-  class?: HTMLAttributes['class']
-}>()
+  showIcon?: boolean;
+  class?: HTMLAttributes["class"];
+}>();
 
 const width = computed(() => {
   return `${Math.floor(Math.random() * 40) + 50}%`;
-})
+});
 </script>
 
 <template>
   <div
     data-sidebar="menu-skeleton"
-    :class="cn('rounded-md h-8 flex gap-2 px-2 items-center', props.class)"
+    :class="cn('flex h-8 items-center gap-2 rounded-md px-2', props.class)"
   >
     <Skeleton
       v-if="showIcon"
@@ -25,7 +25,7 @@ const width = computed(() => {
     />
 
     <Skeleton
-      class="h-4 flex-1 max-w-[--skeleton-width]"
+      class="h-4 max-w-(--skeleton-width) flex-1"
       data-sidebar="menu-skeleton-text"
       :style="{ '--skeleton-width': width }"
     />
